ec93385fff3a162c22a1c92406d00cebad043dfe
gusmrtns <gustavomartins9029@gmail.com>
2024-09-24 16:58:16 -0300

    :feat: create routes for the pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
   return (
     <main>
-      Hello World!!!!
+      <Header />
+
     </main>
   )
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+
+
+const Header = () => {
+
+    // some logic here
+
+    return (
+
+        <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+            <Toolbar sx={{ display: "flex" }}>
+
+            </Toolbar>
+        </AppBar>
+
+    );
+
+};
+
+
+
+export default Header;
